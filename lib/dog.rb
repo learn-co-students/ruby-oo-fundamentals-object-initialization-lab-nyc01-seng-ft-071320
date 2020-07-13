6e6e0aec8277df2b009b71ff41e7bf0e96c1a4e0
vincentyang01 <vincentyangcsi@gmail.com>
2020-07-13 17:32:05 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+    def initialize(name="something", breed="Mutt")
+        @breed = breed
+        @name = name
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+    def breed
+        @breed
+    end
+    def name=(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+
+end
